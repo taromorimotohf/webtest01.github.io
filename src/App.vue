4f4222c169f9f65ac59dddbd181cba6782a79259
森本太郎 <taro_morioto@hfod-022.hot-factory.local>
2018-05-14 19:06:34 +0900

    pwa の準備

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-ons-page id="app">
+    <v-ons-splitter>
+      <v-ons-splitter-side swipeable collapse width="250px"
+        :animation="$ons.platform.isAndroid() ? 'overlay' : 'reveal'"
+        :open.sync="menuIsOpen">
+        <menu-page></menu-page>
+      </v-ons-splitter-side>
+
+      <v-ons-splitter-content>
+        <home-page @toggleMenu="menuIsOpen = !menuIsOpen"></home-page>
+      </v-ons-splitter-content>
+    </v-ons-splitter>
+  </v-ons-page>
+</template>
+
+<script>
+import HomePage from './pages/HomePage'
+import MenuPage from './pages/MenuPage'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      menuIsOpen: false
+    }
+  },
+  components: {
+    HomePage,
+    MenuPage
+  }
+}
+</script>
+
+<style>
+ons-splitter-side[side=left][animation=overlay] {
+  border-right: 1px solid #BBB;
+}
+</style>
